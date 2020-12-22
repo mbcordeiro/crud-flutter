433e1b1dd08b53242f6eded3caf5d042da083a8c
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-21 22:43:31 -0300

    Insert and update

--- lib/provider/users_provider.dart
+++ lib/provider/users_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:crud/data/dammy_user.dart';
 import 'package:crud/models/user.dart';
 import 'package:flutter/material.dart';
@@ -15,5 +17,34 @@ class UsersProvider with ChangeNotifier {
 
   User byIndex(int index) {
     return _items.values.elementAt(index);
+  }
+
+  void put(User user) {
+    if (user == null) {
+      return;
+    }
+
+    if (user.id != null &&
+        user.id.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
+      _items.update(
+          user.id,
+          (_) => User(
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              avatarUrl: user.avatarUrl));
+    } else {
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+          id,
+          () => User(
+              id: id,
+              name: user.name,
+              email: user.email,
+              avatarUrl: user.avatarUrl));
+    }
+
+    notifyListeners();
   }
 }
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,4 +1,5 @@
 import 'package:crud/components/user_tile.dart';
+import 'package:crud/models/user.dart';
 import 'package:crud/provider/users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +11,14 @@ class UserList extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('User List'),
-          actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  users.put(User(
+                      name: 'Teste', email: 'teste@email.com', avatarUrl: ''));
+                })
+          ],
         ),
         body: ListView.builder(
           itemCount: users.count,
