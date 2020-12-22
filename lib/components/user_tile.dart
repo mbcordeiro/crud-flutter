4f4e0e246f7f39505a8f25898d41d224581b9e0f
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-22 01:52:21 -0300

    Delete user

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,8 @@
 import 'package:crud/models/user.dart';
+import 'package:crud/provider/users_provider.dart';
 import 'package:crud/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -23,13 +25,33 @@ class UserTile extends StatelessWidget {
               icon: Icon(Icons.edit),
               color: Colors.blueAccent,
               onPressed: () {
-                Navigator.of(context).pushNamed(AppRoutes.USER_FORM, arguments: user);
+                Navigator.of(context)
+                    .pushNamed(AppRoutes.USER_FORM, arguments: user);
               },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Deseja excluir usuário?'),
+                    actions: [
+                      FlatButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      FlatButton(
+                          child: Text('Sim'),
+                          onPressed: () => Navigator.of(context).pop(true))
+                    ],
+                  ),
+                ).then((confirmed) =>
+                    Provider.of<UsersProvider>(context, listen: false)
+                        .remove(user));
+              },
             )
           ],
         ),
