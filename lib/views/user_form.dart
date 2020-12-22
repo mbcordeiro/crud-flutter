ed3c8629fa2882afb7a60d993a97c080d8d1a624
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-22 01:37:15 -0300

    Update user

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -7,8 +7,19 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, Object> _formData = {};
 
+  void _loadFormData(User user) {
+    if (user != null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final User user = ModalRoute.of(context).settings.arguments;
+    _loadFormData(user);
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
@@ -41,14 +52,31 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
+                initialValue: _formData['name'],
                 decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Nome inválido';
+                  }
+
+                  return null;
+                },
                 onSaved: (value) => _formData['name'] = value,
               ),
               TextFormField(
+                initialValue: _formData['email'],
                 decoration: InputDecoration(labelText: 'E-mail'),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'E-mail inválido';
+                  }
+
+                  return null;
+                },
                 onSaved: (value) => _formData['email'] = value,
               ),
               TextFormField(
+                initialValue: _formData['avatarUrl'],
                 decoration: InputDecoration(labelText: 'URL do Avatar'),
                 onSaved: (value) => _formData['avatarUrl'] = value,
               ),
