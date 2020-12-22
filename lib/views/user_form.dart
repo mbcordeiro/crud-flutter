4318028862c3fdf0f95675a3ef1c61fbab10efc9
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-22 01:25:24 -0300

    Insert user

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,11 +1,60 @@
+import 'package:crud/models/user.dart';
+import 'package:crud/provider/users_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, Object> _formData = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final isValid = _form.currentState.validate();
+
+              if (isValid) {
+                _form.currentState.save();
+                Provider.of<UsersProvider>(context).put(
+                  User(
+                      id: _formData['id'],
+                      name: _formData['name'],
+                      email: _formData['email'],
+                      avatarUrl: _formData['avatarUrl']),
+                );
+              }
+
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
